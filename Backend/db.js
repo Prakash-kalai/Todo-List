35d44833f673a10d5bc7bffd419ff62ec3f599b8
Prakash K <jkprakash342@gmail.com>
2025-02-10 22:13:21 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/todoList")
+mongoose.connect("mongodb+srv://Prakash:root123@cluster0.9ctws.mongodb.net/App")
 .then("successfully")
 .catch(error => handleError(error));
 
